e752ebb2cead0318aa8b91ec7c90dfbaeedf8040
Imemyslf <kishanbro9666@gmail.com>
2023-11-02 23:29:43 +0530

    addedtextbox

--- rayz_gst.py
+++ rayz_gst.py
@@ -7,5 +7,8 @@ app = CTk()
 message_1 = customtkinter.CTkLabel(app,text="Rayz_GST")
 message_1.grid(row=0,column=0)
 
+text_box_1 = customtkinter.CTkTextbox(app,border_width=2)
+# text_box_1.get("0.0","")
+text_box_1.grid(row=1,column=0)
 
 app.mainloop()
