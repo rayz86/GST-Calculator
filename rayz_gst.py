ea2cc0501c370db83ed300a592e2072ee5418829
Imemyslf <kishanbro9666@gmail.com>
2023-11-02 23:19:35 +0530

    addedCtk

---
+++ rayz_gst.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import customtkinter
+from customtkinter import *
+
+app = CTk()
+
+message_1 = customtkinter.CTkLabel(app,text="Rayz_GST")
+message_1.grid(row=0,column=0)
+
+
+app.mainloop()
